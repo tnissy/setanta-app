adc1a03d8ad17133a762464219fd6b71128c7ae1
tnissy <mumei.f@yandex.com>
2025-02-17 18:40:01 +0900

    白画面なおった

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
 import LoginScreen from './src/screens/LoginScreen';
 import FirestoreTestScreen from './src/screens/FirestoreTestScreen';
 import { NavigationContainer } from '@react-navigation/native';
@@ -10,21 +9,21 @@ const Stack = createStackNavigator();
 const App: React.FC = () => {
   console.log('App component rendered');
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="LoginScreen">
-          <Stack.Screen name="Home" component={FirestoreTestScreen} options={{ title: 'ホーム' }} />
-          <Stack.Screen name="Details" component={LoginScreen} options={{ title: 'FirestoreTestScreen' }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ title: 'ログイン' }} 
+        />
+        <Stack.Screen 
+          name="Home" 
+          component={FirestoreTestScreen} 
+          options={{ title: 'ホーム' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
